5adaf3b3b47bbf0e66ef797de2bd2dd514c4d3c1
yudongwei <yudongwei@greenlandfinancial.com>
2023-10-21 13:00:35 +0800

    init-project,add index-page and gateway route check

--- spring-boot-front/src/main/java/com/deer/front/controller/IndexController.java
+++ spring-boot-front/src/main/java/com/deer/front/controller/IndexController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class IndexController {
 
-    @GetMapping ("/")
+    @GetMapping ("/abc")
     public String index(){
         return "hello,front ok";
     }
